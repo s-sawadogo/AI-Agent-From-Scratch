02f990731b7895fecd236ca642a0e2d376b16541
s-sawadogo <saibasaw@gmail.com>
2025-08-26 23:04:31 +0200

    main file upadated

--- main.py
+++ main.py
@@ -13,26 +13,6 @@ def calculator(a: float, b: float) -> str:
     print("Calculator tool has been called.")
     return f"The sum of {a} and {b} is {a + b}"
 
-@tool
-def full_calculator(a: float, b: float) -> str:
-    """ Useful for performing basic arithmetic calculations with numbers"""
-    text = input(" Which operation do you want to perform ?")
-    print("Full Calculator tool has been called.")
-    if text.lower() == "sum":
-        a, b = map(float, input(" Enter two numbers separated by space: ").split())
-        return f"The sum of {a} and {b} is {a + b}"
-    elif text.lower() == "difference":
-        a = float(input(" Enter the greater number: "))
-        b = float(input(" Enter the smaller number: "))
-        return f"The difference between {a} and {b} is {a - b}"
-    elif text.lower() == "product":
-        a = float(input(" Enter the first number: "))
-        b = float(input(" Enter the second number: "))
-        return f"The product of {a} and {b} is {a * b}"
-    elif text.lower() == "quotient":
-        a = float(input(" Enter the numerator: "))
-        b = float(input(" Enter the denominator: "))
-        return f"The quotient of {a} and {b} is {a / b if b != 0 else 'Division by zero error'}"
 
 # Tool for greeting
 @tool
@@ -45,7 +25,7 @@ def say_hello(name: str) -> str:
 def main():
     model = ChatOpenAI(temperature=0) # no random model
 
-    tools = [calculator, full_calculator, say_hello]
+    tools = [calculator, say_hello]
     agent_executor = create_react_agent(model, tools)
 
     print(f"Welcome! I'm your AI assistant. Type 'quit' to exit.")
